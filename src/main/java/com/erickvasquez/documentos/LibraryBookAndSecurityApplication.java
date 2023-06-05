527a581f822211e297565f64b523c7e783416bf7
ErickVasquez11 <00065520@uca.edu.sv>
2023-06-04 21:43:13 -0600

    :sparkles: Add package

---
+++ src/main/java/com/erickvasquez/documentos/LibraryBookAndSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.erickvasquez.documentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookAndSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBookAndSecurityApplication.class, args);
+	}
+
+}
